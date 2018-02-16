233d98a3a006ceed92af7fa7b733a52944ce3bfc
David Wallis <david@wallis2000.co.uk>
2018-02-16 11:19:16 +0000

    replace tabs with spaces

--- prebuild.ps1
+++ prebuild.ps1
@@ -1,14 +1,14 @@
 [CmdLetBinding()]
 param
 (   
-	[Parameter(Mandatory = $true, Position = 0)]
-	[string]$TargetDir,
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$TargetDir,
 
-	[Parameter(Mandatory = $true, Position = 1)]
-	[string]$ProjectName,
-	
-	[Parameter(Mandatory = $true, Position = 2)]
-	[string]$ProjectDir
+    [Parameter(Mandatory = $true, Position = 1)]
+    [string]$ProjectName,
+    
+    [Parameter(Mandatory = $true, Position = 2)]
+    [string]$ProjectDir
 
 )
 
